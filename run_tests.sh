1a6624b4c1c47a1b668c1abc0c9c8938aac3bd31
Jason Burmark <mr.burmark@gmail.com>
2014-10-27 14:38:35 -0700

    improved test script

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,18 @@
 
 mpicc mpFdiff.c Fdutils.c -O3 -o mpFd -lm
 
-for file in G1202 G1602 G2402 G3202 G4802; do
+for file in G1202 G2402 G4802; do
+	echo running test $file
+	for p in 1 4 9 16; do
+		echo running test with $p procs
+		for i in 1 2 3 4 5; do
+			echo test $i
+			mpirun -n $p ./mpFd $file >> test.log
+		done
+	done
+done
+
+for file in G1602 G3202; do
 	echo running test $file
 	for p in 1 4 16; do
 		echo running test with $p procs
@@ -12,3 +23,4 @@ for file in G1202 G1602 G2402 G3202 G4802; do
 		done
 	done
 done
+
